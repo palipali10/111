1d21d465fc9982a5a84f09b94c602ddaf1fc6e31
palipali <palermocagilari10@gmail.com>
2026-05-04 00:43:57 -0700

    Add files via upload

---
+++ 3Body/src/main.c
@@ -0,0 +1,21 @@
+#define SDL_MAIN_HANDLED
+#include <SDL3/SDL.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/*
+    draw to screen
+    physics engine
+        movement
+    simulate 3 body problem
+*/
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
